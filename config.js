67e1278a77424ed26ca0af7d9778c0a0d0a9251a
mutifix <139007103+mutifix@users.noreply.github.com>
2024-02-18 22:52:26 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,22 +1,22 @@
 module.exports = {
 
-    BotToken: "MTIwNTgxOTU2NTcxNjUzNzQyNA.Gq3-sL.R6oJbGRM1ml8SCbAEV3vMObZVXmnx3x6E6ydCE",//Bot Token
-    ServerID: "1208337105340862534",// Server Id
+    BotToken: "",//Bot Token
+    ServerID: "",// Server Id
     Prefix: ["."],// botun öneki
     BotOwners: ["1179280051443867728"],// bot owners
-    MongoURL: "mongodb+srv://ertununannesi:ertununannesi@cluster0.6oyu9nd.mongodb.net/priw?retryWrites=true&w=majority", //mongo url
-    BotVoiceChannel: "1208745160440021043", //Botun Bağlacağı Ses Kanal IDsi
+    MongoURL: "", //mongo url
+    BotVoiceChannel: "", //Botun Bağlacağı Ses Kanal IDsi
     BotStatus: "muti was here", // Bot Durum
 
-    MemberRole: "1208390389539672086",//member role
-    SecretRoomCategory: "1208745276051955782",//secret category
-    ChatChannel: "1208535600236339240",//chat channel
-    WelcomeChannel: "1208535600236339240",//Welcome Channel
+    MemberRole: "",//member role
+    SecretRoomCategory: "",//secret category
+    ChatChannel: "",//chat channel
+    WelcomeChannel: "",//Welcome Channel
 
 
     // bunlar ship icin önemli erkek kız rolü filan varsa atayin
-    ManRoles: "1208390390344847410",//erkek rol 
-    WomanRoles: "1208390391360000090",//kız rol 
+    ManRoles: "",//erkek rol 
+    WomanRoles: "",//kız rol 
 }
 
 
